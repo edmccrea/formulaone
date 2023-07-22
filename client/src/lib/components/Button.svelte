d4cf9e741bba7716efc000b25401e8d0a569ff05
edmccrea <ed-mccrea@hotmail.co.uk>
2023-07-22 22:24:17 +0200

    refactor: styling

--- client/src/lib/components/Button.svelte
+++ client/src/lib/components/Button.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   export let type: "button" | "submit" | "reset" = "button";
+  export let fullWidth = false;
 </script>
 
 <button
   on:click
   {type}
-  class="bg-blue-500 hover:bg-blue-700 rounded-md py-1 px-2 w-fit"
+  class="bg-blue-500 hover:bg-blue-700 rounded-md py-1 px-2 {fullWidth
+    ? 'w-full'
+    : 'w-fit'}"
 >
   <slot />
 </button>
