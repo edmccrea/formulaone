926d24a2be9c66fd7ef2f463b0bb8d017cdfcc43
edmccrea <ed-mccrea@hotmail.co.uk>
2023-07-27 21:48:24 +0200

    feat: add types

--- client/src/app.d.ts
+++ client/src/app.d.ts
@@ -1,9 +1,27 @@
 declare global {
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
-    // interface Platform {}
+    interface DatabaseRace {
+      race_id: number;
+      race_name: string;
+      race_type: string;
+      country_flag: string;
+      qualifying_start: Date;
+      location: string;
+      track_name: string;
+      race_start: Date;
+      race_image: string;
+    }
+    interface Race {
+      id: number;
+      name: string;
+      type: string;
+      flag: string;
+      qualyStart: Date;
+      location: string;
+      track: string;
+      raceStart: Date;
+      image: string;
+    }
   }
 }
 
