4f9cdc25a3b64f89076656605d8408352a0403f1
edmccrea <ed-mccrea@hotmail.co.uk>
2023-07-27 21:48:57 +0200

    feat: map races

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -15,8 +15,8 @@ export const load = (async ({ cookies, fetch }) => {
     },
   });
 
-  const races = await res.json();
-  console.log("races", races);
+  const races: App.DatabaseRace[] = await res.json();
+  const mappedRaces = mapRaces(races);
 
   const userReq = await fetch("/api/user", {
     method: "GET",
@@ -27,10 +27,25 @@ export const load = (async ({ cookies, fetch }) => {
   });
 
   const user = await userReq.json();
-  console.log("user", user);
 
   return {
-    races,
+    races: mappedRaces,
     user,
   };
 }) satisfies PageServerLoad;
+
+function mapRaces(races: App.DatabaseRace[]): App.Race[] {
+  return races.map((race) => {
+    return {
+      id: race.race_id,
+      name: race.race_name,
+      type: race.race_type,
+      flag: race.country_flag,
+      qualyStart: race.qualifying_start,
+      location: race.location,
+      track: race.track_name,
+      raceStart: race.race_start,
+      image: race.race_image,
+    };
+  });
+}
