31663c559bc73ae6b5114314aa498fb262214692
edmccrea <ed-mccrea@hotmail.co.uk>
2023-07-30 21:34:41 +0200

    feat: add card for races

--- client/src/lib/components/race/RaceCard.svelte
+++ client/src/lib/components/race/RaceCard.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
-  export let race: any = "test";
+  import { goto } from "$app/navigation";
+
+  export let race: App.Race;
+  const raceDate = new Date(race.raceStart);
+  const dateToDisplay = raceDate.toDateString();
+
+  let isUpcoming = true;
+  let betPlaced = true;
+
+  function goToRace() {
+    goto(`/race?id=${race.id}`);
+  }
 </script>
 
-<div>
-  <img src="" alt="" />
-  <h3>Race Title</h3>
-  <p>Race type</p>
-  <p>Date</p>
+<div
+  on:click={goToRace}
+  on:keyup={goToRace}
+  class="bg-zinc-900 border border-slate-700 rounded-md w-72 h-80 hover:cursor-pointer hover:bg-zinc-800 transition-all ease-in-out duration-300 shrink-0 snap-start"
+>
+  <div class="h-3/5">
+    <img src={race.image} alt="" class="w-full h-full object-cover" />
+  </div>
+  <div class="p-4">
+    <div class="flex items-center gap-2">
+      <img src={race.flag} alt="" class="h-4" />
+      <h3 class="text-xl font-bold">{race.name}</h3>
+    </div>
+    <p>{race.type}</p>
+    <div class="flex justify-between items-center">
+      <p class="text-sm mt-4">{dateToDisplay}</p>
+      {#if isUpcoming && !betPlaced}
+        <p
+          class="w-fit mt-2 py-1 px-2 bg-yellow-200 text-yellow-900 rounded-md text-sm"
+        >
+          Bet not placed
+        </p>
+      {:else if isUpcoming && betPlaced}
+        <p
+          class="w-fit mt-2 py-1 px-2 bg-green-200 text-green-900 rounded-md text-sm"
+        >
+          Bet placed
+        </p>
+      {/if}
+    </div>
+  </div>
 </div>
