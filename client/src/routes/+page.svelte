2e6b5a3c00e54461908060075ecb4286ae2b6b31
edmccrea <ed-mccrea@hotmail.co.uk>
2023-07-30 21:35:44 +0200

    refactor: update pages

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+  import { fade } from "svelte/transition";
+
   import Button from "$lib/components/Button.svelte";
   import RaceCard from "$lib/components/race/RaceCard.svelte";
 
   export let data;
-  const username = data.user.username;
-  const races = data.races;
+  const user = data.user;
+  const races: App.Race[] = data.races;
+  const positionSuffix =
+    user.position === 1
+      ? "st"
+      : user.position === 2
+      ? "nd"
+      : user.position === 3
+      ? "rd"
+      : "th";
 
   async function getAllUsers() {
     const url = "/api/users";
@@ -22,15 +32,20 @@
 </script>
 
 <div
-  class="w-full h-full mx-4 lg:mx-auto max-w-sm lg:max-w-4xl mt-12 flex flex-col flex-1"
+  in:fade
+  class="w-full h-full mx-4 md:mx-auto max-w-sm md:max-w-2xl lg:max-w-6xl mt-12 flex flex-col flex-1"
 >
   <h1 class="text-5xl font-bold mb-4">Formula One Bets 2023</h1>
-  <p>Hello {username}, you're currently in nth place with n points</p>
+  <p>
+    Hello {user.username}, you're currently in {user.position}{positionSuffix} place
+    with {user.points}
+    points.
+  </p>
 
   <div class="mt-4">
-    <h2 class="font-bold text-2xl">Upcoming Race</h2>
-    <p>If race is this week, then display a countdown</p>
-    <RaceCard />
+    <h2 class="font-bold text-2xl mb-4">Upcoming Race</h2>
+    <!-- If race is this week, then display a countdown -->
+    <RaceCard race={races[0]} />
   </div>
 
   <div class="mt-4">
@@ -38,11 +53,21 @@
   </div>
 
   <div class="mt-4">
-    <h2 class="font-bold text-2xl">Previous Races</h2>
+    <h2 class="font-bold text-2xl mb-4">Future Races</h2>
+    <div class="flex gap-4 w-full relative overflow-auto snap-x">
+      {#each races as race}
+        <RaceCard {race} />
+      {/each}
+    </div>
   </div>
 
   <div class="mt-4">
-    <h2 class="font-bold text-2xl">Previous Races</h2>
+    <h2 class="font-bold text-2xl mb-4">Previous Races</h2>
+    <div class="flex gap-4 w-full relative overflow-auto">
+      {#each races as race}
+        <RaceCard {race} />
+      {/each}
+    </div>
   </div>
 
   <div class="mt-12">
@@ -51,7 +76,6 @@
       <a href="/login" class="w-fit">Go to login page</a>
 
       <Button on:click={getAllUsers}>Get all users</Button>
-      <Button on:click={getAllRaces}>Get all races</Button>
 
       <a href="/logout" class="w-fit" data-sveltekit-preload-data="off">
         <Button>Logout</Button>
