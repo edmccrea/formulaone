081487203429491233d588295736e261dbc69622
edmccrea <ed-mccrea@hotmail.co.uk>
2023-08-25 11:21:43 +0200

    feat: og data

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -6,6 +6,16 @@
   import Footer from "$lib/components/Footer.svelte";
 </script>
 
+<svelte:head>
+  <meta
+    property="og:image"
+    content="https://cdn.racingnews365.com/_1125x633_crop_center-center_85_none/XPB_18286_HiRes.jpg?v=1668070831"
+  />
+  <meta property="og:title" content="F1 Nerds Bets" />
+  <meta property="og:url" content="https://www.silverstonepitcrew.com" />
+  <meta property="og:type" content="website" />
+</svelte:head>
+
 {#if $page.url.pathname !== "/login" && $page.url.pathname !== "/register"}
   <Navbar />
 {/if}
