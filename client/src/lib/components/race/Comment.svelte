e9531aea72c84776c7d29e1b20456dd191f72440
edmccrea <ed-mccrea@hotmail.co.uk>
2023-09-17 08:11:58 +0200

    refactor: comment date format

--- client/src/lib/components/race/Comment.svelte
+++ client/src/lib/components/race/Comment.svelte
@@ -2,7 +2,18 @@
   import { fade } from "svelte/transition";
 
   export let comment: App.Comment;
-  const stringifiedDate = new Date(comment.created_at).toLocaleString();
+  function formatDate(date: Date) {
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = String(date.getFullYear()).slice(-2);
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  }
+
+  const formattedDate = formatDate(comment.created_at);
+  console.log(formattedDate);
 </script>
 
 <div class="flex gap-4 mt-4" in:fade>
@@ -14,7 +25,7 @@
   <div class="flex flex-col">
     <div class="flex gap-2 items-baseline">
       <span class="font-bold">{comment.username}</span>
-      <span class="text-sm text-slate-500">{stringifiedDate}</span>
+      <span class="text-sm text-neutral-400">{formattedDate}</span>
     </div>
     <p class="text-slate-100">{comment.comment}</p>
   </div>
