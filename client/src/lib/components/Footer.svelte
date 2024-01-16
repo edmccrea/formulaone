e00036f3f407f251070cc553f5995148c911686a
edmccrea <ed-mccrea@hotmail.co.uk>
2024-01-16 21:01:57 +0100

    update footer and navbar

--- client/src/lib/components/Footer.svelte
+++ client/src/lib/components/Footer.svelte
@@ -1,3 +1,5 @@
-<footer class="w-full py-6 border-t border-t-gray-400 flex justify-center items-center">
-    <p class="text-sm font-light">Formula One Bets 2023</p>
+<footer
+  class="w-full py-6 border-t border-t-gray-400 flex justify-center items-center"
+>
+  <p class="text-sm font-light">Formula One Bets 2024</p>
 </footer>
--- client/src/lib/components/Navbar.svelte
+++ client/src/lib/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import Button from "./Button.svelte";
+  import { selectedYear } from "../../stores/selected-year";
 
   async function handleLogout() {
     const res = await fetch("/api/logout", {
@@ -11,15 +12,22 @@
       goto("/login");
     }
   }
+
+  function setYear(year: number) {
+    selectedYear.set(year);
+    goto("/");
+  }
 </script>
 
 <nav
-  class="w-full py-6 border-b border-b-gray-400 fixed backdrop-blur-md bg-[#121212]/60 z-50"
+  class="w-full py-6 border-b border-b-gray-400 fixed backdrop-blur-md bg-[#121212]/60 z-50 flex justify-between px-8"
 >
-  <ul class="flex justify-between px-8">
-    <a href="/" class="flex items-center">
-      <img src="/f1.png" alt="" class="h-6" /></a
-    >
+  <a href="/" class="flex items-center">
+    <img src="/f1.png" alt="" class="h-6" /></a
+  >
+  <ul class="flex gap-4 items-center">
+    <li on:click={() => setYear(2023)}>2023</li>
+    <li on:click={() => setYear(2024)}>2024</li>
     <li><Button on:click={handleLogout}>Logout</Button></li>
   </ul>
 </nav>
