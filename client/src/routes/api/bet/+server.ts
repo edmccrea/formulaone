2fb8a8011412b1ad477abd93e88b7b3260ec7c5e
edmccrea <ed-mccrea@hotmail.co.uk>
2024-02-29 22:58:37 +0100

    test new bet update

--- client/src/routes/api/bet/+server.ts
+++ client/src/routes/api/bet/+server.ts
@@ -11,14 +11,23 @@ export const POST: RequestHandler = async ({ request }) => {
     .from(bets)
     .where(and(eq(bets.userId, betReq.userId), eq(bets.raceId, betReq.raceId)));
   if (existingBet) {
-    await db
+    const insertedBet = await db
       .update(bets)
       .set({ first: betReq.first, second: betReq.second, third: betReq.third })
       .where(
         and(eq(bets.userId, betReq.userId), eq(bets.raceId, betReq.raceId))
-      );
+      )
+      .returning({
+        betId: bets.betId,
+        userId: bets.userId,
+        raceId: bets.raceId,
+        seasonId: bets.seasonId,
+        first: bets.first,
+        second: bets.second,
+        third: bets.third,
+      });
 
-    return new Response("Bet updated", { status: 200 });
+    return new Response(JSON.stringify({ insertedBet }), { status: 200 });
   } else {
     type NewBet = typeof bets.$inferInsert;
     const insertBet = async (bet: NewBet) => {
