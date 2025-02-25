f06be8bfc2a8aefa462aa4ec449355734291d9b1
edmccrea <ed-mccrea@hotmail.co.uk>
2025-02-25 17:29:21 +0100

    use current date in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer
   class="w-full py-6 border-t border-t-gray-400 flex justify-center items-center"
 >
-  <p class="text-sm font-light">Formula One Bets 2024</p>
+  <p class="text-sm font-light">Formula One Bets {new Date().getFullYear()}</p>
 </footer>
